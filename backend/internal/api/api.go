067f46d2a270c918a188073220ff7f71f1e61df7
Ki4EH <Poka132@yandex.ru>
2024-03-26 21:57:06 +0300

    Api NewUser, for adding new User

--- backend/internal/api/api.go
+++ backend/internal/api/api.go
@@ -1,1 +1,39 @@
 package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/jenkinsyoung/web_music_sanctuary/internal/hash"
+	"github.com/jenkinsyoung/web_music_sanctuary/internal/models"
+	"log"
+	"net/http"
+)
+
+func NewUser(w http.ResponseWriter, r *http.Request) {
+	user := models.User{}
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		log.Printf("Error parsing json: %s", err)
+	}
+	user.Password = hash.PasswordHash(user.Password)
+	//TODO: Отправить JWT
+	//TODO: нужно добавить бользователя в базу данных
+	fmt.Println(user)
+	w.WriteHeader(http.StatusOK)
+}
+
+func LoggingUser(w http.ResponseWriter, r *http.Request) {
+	user := models.User{}
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		log.Printf("Error parsing json: %s", err)
+	}
+	//TODO: написать авторизацию
+}
+
+//TODO: напистаь отправку объявлений по БОЛЬШОЙ категории
+
+func SetupRoutes() http.Handler {
+	router := mux.NewRouter()
+	router.HandleFunc("/api/user", NewUser).Methods("POST")
+	return router
+}
