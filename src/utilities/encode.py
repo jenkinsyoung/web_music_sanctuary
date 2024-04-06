d1dfd7c5315c818724ab0ab710194a37ca217c80
sv022 <ssvatkin04@gmail.com>
2024-04-06 15:59:30 +0300

    fixed file path

--- src/utilities/encode.py
+++ src/utilities/encode.py
@@ -1,5 +1,5 @@
 from base64 import b64decode
 
 def decode_image(image : str):
-    with open("src/yolov9/data/images/horses.png", "wb") as fh:
+    with open("src/yolov9/data/images/image.png", "wb") as fh:
         fh.write(b64decode(image))
