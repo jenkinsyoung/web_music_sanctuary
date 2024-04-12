4783284a0aa3453b0c4c333f13e1d87ff2993419
sv022 <ssvatkin04@gmail.com>
2024-04-12 14:52:58 +0300

    added category classification

--- src/utilities/predictionparse.py
+++ src/utilities/predictionparse.py
@@ -1,25 +1,25 @@
 classes = [
-    ('classical', 'guitar_form'), 
-    ('concert', 'guitar_form'), 
-    ('dreadnought', 'guitar_form'), 
-    ('es', 'guitar_form'), 
-    ('explorer', 'guitar_form'), 
-    ('flying-v', 'guitar_form'), 
+    ('classical', 'guitar_form', 'acoustic'), 
+    ('concert', 'guitar_form', 'acoustic'), 
+    ('dreadnought', 'guitar_form', 'acoustic'), 
+    ('es', 'guitar_form', 'electric'), 
+    ('explorer', 'guitar_form', 'electric'), 
+    ('flying-v', 'guitar_form', 'electric'), 
     ('h-pickup', 'pickup_config'), 
     ('hh-pickup', 'pickup_config'), 
     ('hs-pickup', 'pickup_config'), 
     ('hsh-pickup', 'pickup_config'), 
-    ('les-paul', 'guitar_form'), 
-    ('mustang', 'guitar_form'), 
-    ('parlor', 'guitar_form'), 
-    ('sg', 'guitar_form'), 
+    ('les-paul', 'guitar_form', 'electric'), 
+    ('mustang', 'guitar_form', 'electric'), 
+    ('parlor', 'guitar_form', 'electric'), 
+    ('sg', 'guitar_form', 'electric'), 
     ('ss-pickup', 'pickup_config'), 
     ('ssh-pickup', 'pickup_config'), 
     ('sss-pickup', 'pickup_config'), 
-    ('stratocaster', 'guitar_form'), 
-    ('superstrat', 'guitar_form'), 
-    ('telecaster', 'guitar_form'), 
-    ('triple-o', 'guitar_form')
+    ('stratocaster', 'guitar_form', 'electric'), 
+    ('superstrat', 'guitar_form', 'electric'), 
+    ('telecaster', 'guitar_form', 'electric'), 
+    ('triple-o', 'guitar_form', 'acoustic')
 ]
 source = 'src/yolov9/runs/detect/exp4/labels/image.txt'
 
@@ -43,8 +43,19 @@ def parsepred() -> dict:
             res[classes[d][1]] = max((d, detections[d][1]), res[classes[d][1]], key= lambda x: x[1])
         except Exception:
             res[classes[d][1]] = (d, detections[d][1])
+            
+    res['category'] = classes[res['guitar_form'][0]][2]
         
     for k, v in res.items(): # example: {'guitar_form': (18, '0.51875'), 'pickup_config': (7, '0.626042')}
+        if k == 'category': continue
         res[k] = classes[v[0]][0]
+        
+    try:
+        res['pickup_config']
+    except KeyError:
+        res['pickup_config'] = None
     
     return res
+
+
+print(parsepred())
