2a4db385780350eb3f0dccf57e792f059e951eab
Anastasia Poloshkova <anpoloshkova@mail.ru>
2024-04-23 23:03:58 +0300

    modified:   src/ui/Product.tsx

--- src/ui/Product.tsx
+++ src/ui/Product.tsx
@@ -5,7 +5,7 @@ const Product = ({product} : any) => {
     return (
         <div className={styles.container}>
             <h1>{product.guitar_name}</h1>
-            {product.img_list != null ? <div className={styles.img} style={{backgroundImage: `url(/${product.img_list[0].image})`}} />:<div className={styles.img} />}
+            {product.img_list != null ? <div className={styles.img} style={{backgroundImage: `url(${product.img_list[0].image})`}} />:<div className={styles.img} />}
             
             <div className={styles.tags}>#{product.pickup_config} #{product.guitar_form}</div>
             <div className={styles.cost}><p>Цена:</p><span>{product.cost} ₽</span></div>
